3fbd2a09b3e0aeb6f3887778c7591e508e110e33
Luo <379430@protomail.com>
2022-08-23 02:43:25 +0100

    调整styles

---
+++ client/src/styles.js
@@ -0,0 +1,19 @@
+import { makeStyles } from "@material-ui/core";
+
+export default makeStyles(() => ({
+  appBar: {
+    borderRadius: 15,
+    margin: "30px 0",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#FFFCF6",
+  },
+  heading: {
+    color: "#402401",
+  },
+  image: {
+    marginLeft: "15px",
+  },
+}));
