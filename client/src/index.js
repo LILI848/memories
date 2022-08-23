3f84b25922eb432b25b6ed37d302aff938528766
Luo <lostxi@hotmail.com>
2022-08-23 03:27:49 +0100

    test

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,7 @@
 import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
+import { createStore, applyMiddleware, componse } from "redux";
+import thunk from "redux-thunk";
 
 ReactDOM.render(<App />, document.getElementById("root"));
